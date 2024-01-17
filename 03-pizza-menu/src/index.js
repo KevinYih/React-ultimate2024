b0b85f173b4c75528f84ebb4512f95097a6c48f0
keviny <kevinyih2022@gmail.com>
2024-01-16 21:56:06 -0800

    project steps starter

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -78,11 +78,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          {/*react fragment*/}
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later.</p>
       )}
@@ -107,17 +114,21 @@ function Menu() {
     </main>
   );
 }
-//function Pizza(props)  === function Pizza({pizzaObj})  , props = {pizzaObj}
-function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null; //so beautiful. only one return can be allowed!!
+//function Pizza(props)  === function Pizza({pizzaObj})  , props === {pizzaObj}
+function Pizza({ pizzaObj, key }) {
+  //if (pizzaObj.soldOut) return null; //so beautiful. only one return can be allowed!!
   return (
-    <li className="pizza">
+    <li className={pizzaObj.soldOut ? "pizza sold-out" : "pizza"}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
-
         <p>{pizzaObj.ingredients}</p>
-        <span>{Number(pizzaObj.price)}</span>
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{Number(pizzaObj.price)}</span>
+        )} */}
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : Number(pizzaObj.price)}</span>
       </div>
     </li>
   );
