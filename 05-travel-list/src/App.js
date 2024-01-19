00d16bf60ae2f8c79509d43629aaf2fdc64a03c3
keviny <kevinyih2022@gmail.com>
2024-01-19 01:30:01 -0800

    state lift up

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,20 +1,45 @@
 import { useState } from "react";
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Socks3", quantity: 12, packed: false },
-  { id: 4, description: "Socks4", quantity: 12, packed: true },
-  { id: 5, description: "Socks5", quantity: 12, packed: false },
+  // { id: 1, description: "Passports", quantity: 2, packed: false },
+  // { id: 2, description: "Socks", quantity: 12, packed: false },
+  // { id: 3, description: "Socks3", quantity: 12, packed: false },
+  // { id: 4, description: "Socks4", quantity: 12, packed: true },
+  // { id: 5, description: "Socks5", quantity: 12, packed: false },
 ];
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
+  //setFunction must return a items that be changed!
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]); //items is immutable, We can not use array.
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handlePackItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     //4 sibling components
+    // pass a function handleAddItems as a props to Form.
+    // pass a property items as a props to PackingList.
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onPackItems={handlePackItems}
+      />
       <Stats />
     </div>
   );
@@ -24,7 +49,7 @@ function Logo() {
   return <h1>🌴FAR AWAY👜</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setquantity] = useState(1);
 
@@ -36,11 +61,14 @@ function Form() {
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
 
+    onAddItems(newItem);
+
     setDescription("");
     setquantity(1);
   }
 
   return (
+    // form + onSubmit()
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select
@@ -66,25 +94,41 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItems, onPackItems }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItems={onDeleteItems}
+            onPackItems={onPackItems}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItems, onPackItems }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onPackItems(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button
+        onClick={() => {
+          onDeleteItems(item.id);
+        }}
+      >
+        ❌
+      </button>
     </li>
   );
 }
